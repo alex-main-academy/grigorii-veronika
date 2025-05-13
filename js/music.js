e1f885dc933687679a728b9d082ca2ecb9d934fc
alex-main-academy <oleksandrilnytskyi@gmail.com>
2025-05-13 17:55:01 +0300

    change music logic

--- js/music.js
+++ js/music.js
@@ -3,7 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
   const playButton = document.querySelector('.js-play-music');
 
   playButton.addEventListener('click', function () {
-    document.querySelector('.music-play').setAttribute('src', 'img/icons/pause.svg');
-    audio.play();
+    playButton.classList.toggle('active');
+
+    if (playButton.classList.contains('active')) {
+      document.querySelector('.music-play').setAttribute('src', 'img/icons/pause.svg');
+      audio.play();
+    } else {
+      document.querySelector('.music-play').setAttribute('src', 'img/icons/play.svg');
+      audio.pause();
+    }
   });
 });
